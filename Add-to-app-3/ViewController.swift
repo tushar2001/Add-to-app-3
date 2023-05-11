9de977797cfeac79290f411f8652443dfcc93d77
Tushar Tayal <tushar.tayal@olx.com>
2023-05-11 18:28:29 +0530

    Initial Commit

---
+++ Add-to-app-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Add-to-app-3
+//
+//  Created by OLX on 11/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Add-to-app-3UITests/Add_to_app_3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Add_to_app_3UITestsLaunchTests.swift
+//  Add-to-app-3UITests
+//
+//  Created by OLX on 11/05/23.
+//
+
+import XCTest
+
+final class Add_to_app_3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
